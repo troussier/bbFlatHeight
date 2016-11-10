a45c18f98bab2a10a9e0e5383b3881af2920bab9
troussier <troussierd@gmail.com>
2016-11-10 15:12:20 +0900

    box-sizingに対応

--- bbFlatHeight.js
+++ bbFlatHeight.js
@@ -11,26 +11,25 @@
         
         var $this = this;
         $this.css('height', 'auto');
-        var h = 0;
+        var max = 0;
         var col = settings.col || $this.length;
         var row = 0;
         
         $this.each(function(i) {
-            if ($(this).height() > h) {
-                h = $(this).height();
-            }
+            var h = ($(this).css('box-sizing') == 'border-box') ? $(this).outerHeight() : $(this).height();
+            if (h > max) max = h;
             if ((i+1) % col === 0) {
                 var lt = row * col;
                 var gt = lt + (col-1);
                 for (var j=lt; j<=gt; j++) {
-                    $($this[j]).css('height', h);
+                    $($this[j]).css('height', max);
                 }
-                h = 0;
+                max = 0;
                 row++;
             }
         });
         for (var i=$this.length-($this.length % col); i<$this.length; i++) {
-            $($this[i]).css('height', h);
+            $($this[i]).css('height', max);
         }
         
         return this;
