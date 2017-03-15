60eed516a240e07a90276c1c44d1d91e45802eaf
troussier <mori@babagump.co.jp>
2017-03-15 22:36:30 +0900

    デバイスの傾きを変更した際に対応

--- bbFlatHeight.js
+++ bbFlatHeight.js
@@ -8,30 +8,42 @@
         var settings = $.extend({
             col: false
         }, options);
-        
+
         var $this = this;
-        $this.css('height', 'auto');
-        var max = 0;
-        var col = settings.col || $this.length;
-        var row = 0;
-        
-        $this.each(function(i) {
-            var h = ($(this).css('box-sizing') == 'border-box') ? $(this).outerHeight() : $(this).height();
-            if (h > max) max = h;
-            if ((i+1) % col === 0) {
-                var lt = row * col;
-                var gt = lt + (col-1);
-                for (var j=lt; j<=gt; j++) {
-                    $($this[j]).css('height', max);
-                }
-                max = 0;
-                row++;
-            }
-        });
-        for (var i=$this.length-($this.length % col); i<$this.length; i++) {
-            $($this[i]).css('height', max);
+
+        var init = function() {
+            setup();
         }
-        
+
+        var setup = function() {
+            $this.css('height', 'auto');
+            var max = 0;
+            var col = settings.col || $this.length;
+            var row = 0;
+            $this.each(function(i) {
+                var h = ($(this).css('box-sizing') == 'border-box') ? $(this).outerHeight() : $(this).height();
+                if (h > max) max = h;
+                if ((i+1) % col === 0) {
+                    var lt = row * col;
+                    var gt = lt + (col-1);
+                    for (var j=lt; j<=gt; j++) {
+                        $($this[j]).css('height', max);
+                    }
+                    max = 0;
+                    row++;
+                }
+            });
+            for (var i=$this.length-($this.length % col); i<$this.length; i++) {
+                $($this[i]).css('height', max);
+            }
+        }
+
+        $(window).on('orientationchange', function() {
+            setup();
+        });
+
+        init();
+
         return this;
     };
 }(jQuery));
